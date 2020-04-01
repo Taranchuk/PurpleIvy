8fe9e6fd371c231ebb1cfa8cb0f6aa349029af3f
Taranchuk <nfhfyxer@gmail.com>
2020-04-01 09:02:21 +0200

    max distance is 50, new reach check, fixed NRE

--- Source/PurpleIvyDLL/JobGiver_GennyBerserk.cs
+++ Source/PurpleIvyDLL/JobGiver_GennyBerserk.cs
@@ -22,27 +22,28 @@ namespace PurpleIvy
                 return null;
             }
             Pawn pawn2 = this.FindPawnTarget(pawn);
-            Log.Message(pawn + " choose target: " + pawn2.Label);
-
-            if (pawn2 != null && !pawn2.Downed)
+            if (pawn2 != null)
             {
-                Job job2 = JobMaker.MakeJob(JobDefOf.AttackMelee, pawn2);
-                job2.maxNumMeleeAttacks = 1;
-                job2.expiryInterval = Rand.Range(420, 900);
-                job2.canBash = true;
-                Log.Message(pawn + " return attack " + pawn2.Label);
-                return job2;
-            }
-            else if (pawn2 != null && pawn2.Downed)
-            {
-
-                Job job2 = JobMaker.MakeJob(PurpleIvyDefOf.PI_Kill, pawn2);
-                job2.maxNumMeleeAttacks = 1;
-                job2.expiryInterval = Rand.Range(420, 900);
-                job2.canBash = true;
-                job2.killIncappedTarget = true;
-                Log.Message(pawn + " return kill " + pawn2.Label);
-                return job2;
+                Log.Message(pawn + " choose target: " + pawn2.Label);
+                if (!pawn2.Downed)
+                {
+                    Job job2 = JobMaker.MakeJob(JobDefOf.AttackMelee, pawn2);
+                    job2.maxNumMeleeAttacks = 1;
+                    job2.expiryInterval = Rand.Range(420, 900);
+                    job2.canBash = true;
+                    Log.Message(pawn + " return attack " + pawn2.Label);
+                    return job2;
+                }
+                else if (pawn2.Downed)
+                {
+                    Job job2 = JobMaker.MakeJob(PurpleIvyDefOf.PI_Kill, pawn2);
+                    job2.maxNumMeleeAttacks = 1;
+                    job2.expiryInterval = Rand.Range(420, 900);
+                    job2.canBash = true;
+                    job2.killIncappedTarget = true;
+                    Log.Message(pawn + " return kill " + pawn2.Label);
+                    return job2;
+                }
             }
             Log.Message(pawn + " return null");
             return null;
@@ -50,16 +51,17 @@ namespace PurpleIvy
 
         private Pawn FindPawnTarget(Pawn pawn)
         {
-            Predicate<Thing> predicate = (Thing p) => p != null && p != pawn 
-            && p.Faction != pawn.Faction;
-            return (Pawn)GenClosest.ClosestThing_Global(pawn.Position,
-                pawn.Map.mapPawns.AllPawnsSpawned, 200f, predicate);
+            //Predicate<Thing> predicate = (Thing p) => p != null && p != pawn 
+            //&& p.Faction != pawn.Faction;
+            //return (Pawn)GenClosest.ClosestThing_Global(pawn.Position,
+            //    pawn.Map.mapPawns.AllPawnsSpawned, 200f, predicate);
 
-            return (Pawn)AttackTargetFinder.BestAttackTarget(pawn, TargetScanFlags.NeedThreat, delegate (Thing x)
+            return (Pawn)AttackTargetFinder.BestAttackTarget(pawn, TargetScanFlags.NeedReachable, delegate (Thing x)
             {
                 Pawn pawn2 = x as Pawn;
-                return pawn2 != null && pawn2.Spawned;
-            });
+                return pawn2 != null && pawn2.Spawned && pawn.CanReach(pawn, PathEndMode.ClosestTouch, Danger.Deadly, 
+                    false, TraverseMode.ByPawn);
+            }, 0f, 50f);
         }
 
         private const float MaxAttackDistance = 40f;
