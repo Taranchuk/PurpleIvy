1e1562472f70c4d43d6b5b1e831e954a662527d5
Taranchuk <nfhfyxer@gmail.com>
2020-04-15 01:42:43 +0200

    Fix rendering (mostly)

--- Source/PurpleIvyDLL/Sites/WorldLayerRegenerateBiomes.cs
+++ Source/PurpleIvyDLL/Sites/WorldLayerRegenerateBiomes.cs
@@ -9,6 +9,7 @@ namespace PurpleIvy
 {
     public class WorldLayerRegenerateBiomes : WorldLayer
     {
+
         public override IEnumerable Regenerate()
         {
             Log.Message("START");
@@ -21,6 +22,11 @@ namespace PurpleIvy
             {
                 yield break;
             }
+            foreach (object obj2 in this.CalculateInterpolatedVerticesParams())
+            {
+                yield return obj2;
+            }
+            int ind = 0;
             foreach (int num in PurpleIvyData.BiomesToRenderNow)
             {
                 if (num >= 0)
@@ -35,15 +41,9 @@ namespace PurpleIvy
                     int maxCount = this._vertices.Count;
                     while (currentIndex < maxCount)
                     {
-                        if (currentIndex % 1000 == 0)
-                        {
-                            yield return null;
-                        }
-                        if (subMesh.verts.Count > 60000)
-                        {
-                            subMesh = base.GetSubMesh(material);
-                        }
                         subMesh.verts.Add(this._vertices[currentIndex] + this._vertices[currentIndex].normalized * 0.012f);
+                        subMesh.uvs.Add(this.elevationValues[ind]);
+                        ind++;
                         if (currentIndex < maxCount - 2)
                         {
                             subMesh.tris.Add(startVertIndex + currentIndex + 2);
@@ -53,16 +53,96 @@ namespace PurpleIvy
                         int num2 = currentIndex;
                         currentIndex = num2 + 1;
                     }
-                    subMesh.FinalizeMesh(MeshParts.All);
-                    material = null;
-                    subMesh = null;
+                    //subMesh.FinalizeMesh(MeshParts.All);
+                    //material = null;
+                    //subMesh = null;
                     //PurpleIvyData.BiomesToRenderNow.Remove(num);
                 }
+                base.FinalizeMesh(MeshParts.All);
             }
             List<int>.Enumerator enumerator2 = default(List<int>.Enumerator);
+            this.elevationValues.Clear();
+            this.elevationValues.TrimExcess();
             yield break;
             yield break;
         }
+
+        private IEnumerable CalculateInterpolatedVerticesParams()
+        {
+            this.elevationValues.Clear();
+            WorldGrid grid = Find.World.grid;
+            int tilesCount = grid.TilesCount;
+            List<Vector3> verts = grid.verts;
+            List<int> tileIDToVerts_offsets = grid.tileIDToVerts_offsets;
+            List<int> tileIDToNeighbors_offsets = grid.tileIDToNeighbors_offsets;
+            List<int> tileIDToNeighbors_values = grid.tileIDToNeighbors_values;
+            List<Tile> tiles = grid.tiles;
+            int num4;
+            for (int i = 0; i < tilesCount; i = num4 + 1)
+            {
+                if (PurpleIvyData.BiomesToRenderNow.Contains(i))
+                {
+                    Tile tile = tiles[i];
+                    float elevation = tile.elevation;
+                    int num = (i + 1 < tileIDToNeighbors_offsets.Count) ? tileIDToNeighbors_offsets[i + 1] : tileIDToNeighbors_values.Count;
+                    int num2 = (i + 1 < tilesCount) ? tileIDToVerts_offsets[i + 1] : verts.Count;
+                    for (int j = tileIDToVerts_offsets[i]; j < num2; j++)
+                    {
+                        Vector3 vector = default(Vector3);
+                        vector.x = elevation;
+                        bool flag = false;
+                        for (int k = tileIDToNeighbors_offsets[i]; k < num; k++)
+                        {
+                            int num3 = (tileIDToNeighbors_values[k] + 1 < tileIDToVerts_offsets.Count) ? tileIDToVerts_offsets[tileIDToNeighbors_values[k] + 1] : verts.Count;
+                            int l = tileIDToVerts_offsets[tileIDToNeighbors_values[k]];
+                            while (l < num3)
+                            {
+                                if (verts[l] == verts[j])
+                                {
+                                    Tile tile2 = tiles[tileIDToNeighbors_values[k]];
+                                    if (flag)
+                                    {
+                                        break;
+                                    }
+                                    if ((tile2.elevation >= 0f && elevation <= 0f) || (tile2.elevation <= 0f && elevation >= 0f))
+                                    {
+                                        flag = true;
+                                        break;
+                                    }
+                                    if (tile2.elevation > vector.x)
+                                    {
+                                        vector.x = tile2.elevation;
+                                        break;
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    l++;
+                                }
+                            }
+                        }
+                        if (flag)
+                        {
+                            vector.x = 0f;
+                        }
+                        if (tile.biome.DrawMaterial.shader != ShaderDatabase.WorldOcean && vector.x < 0f)
+                        {
+                            vector.x = 0f;
+                        }
+                        this.elevationValues.Add(vector);
+                    }
+                    if (i % 1000 == 0)
+                    {
+                        yield return null;
+                    }
+                }
+                num4 = i;
+            }
+            yield break;
+        }
+
+        private List<Vector3> elevationValues = new List<Vector3>();
 
         private readonly List<Vector3> _vertices = new List<Vector3>();
     }
