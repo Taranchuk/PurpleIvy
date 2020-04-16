5bfd6b3a08aa4950649bc2a909a37a785b8fd9e4
EvaineQ <49371749+EvaineQ@users.noreply.github.com>
2020-04-16 19:35:00 +0200

    Add files via upload

--- Source/PurpleIvyDLL/GenerationWorker/IncidentWorker_GenerationOffset.cs
+++ Source/PurpleIvyDLL/GenerationWorker/IncidentWorker_GenerationOffset.cs
@@ -9,64 +9,31 @@ namespace GenerationWorker
 {
 	public class IncidentWorker_GenerationOffset : IncidentWorker
 	{
+
+        private static readonly IntRange TimeoutDaysRange = new IntRange(9, 14);
+
+        private const int minDist = 8;
+        private const int maxDist = 14;
 		protected override bool CanFireNowSub(IncidentParms parms)
-		{
-			int num;
-			Faction faction;
-			return base.CanFireNowSub(parms) && Find.FactionManager.RandomNonHostileFaction(false, false, false, TechLevel.Industrial) != null && TileFinder.TryFindNewSiteTile(out num) && SiteMakerHelper.TryFindRandomFactionFor(SiteCoreDefOf.OldOutpost, null, out faction, true, null);
-		}
+        {
+            return base.CanFireNowSub(parms) && TileFinder.TryFindNewSiteTile(out int tile, minDist, maxDist, false, true, -1) && CommsConsoleUtility.PlayerHasPoweredCommsConsole();
+        }
 
-		protected override bool TryExecuteWorker(IncidentParms parms)
-		{
-			Faction faction = parms.faction;
-			if (faction == null)
-			{
-				faction = Find.FactionManager.RandomNonHostileFaction(false, false, false, TechLevel.Industrial);
-			}
-			if (faction == null)
-			{
-				return false;
-			}
-			int tile;
-			if (!TileFinder.TryFindNewSiteTile(out tile))
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            if (!TileFinder.TryFindNewSiteTile(out var tile, minDist, maxDist, false, true, -1))
             {
-				return false;
-			}
-			SitePartDef sitePart;
-			Faction siteFaction;
-			if (!SiteMakerHelper.TryFindSiteParams_SingleSitePart(SiteCoreDefOf.OldOutpost, Rand.Chance(1f) ? "SiteTreatSecured" : null, out sitePart, out siteFaction, null, true, null))
-			{
-				return false;
-			}
-			IntRange questSiteTimeoutDaysRange = SiteTuning.QuestSiteTimeoutDaysRange;
-			int randomInRange = questSiteTimeoutDaysRange.RandomInRange;
-			Site site = IncidentWorker_GenerationOffset.CreateSite(tile, sitePart, randomInRange, siteFaction);
-			List<Thing> list = this.GenerateItems(siteFaction, site.desiredThreatPoints);
-			site.GetComponent<ItemStashContentsComp>().contents.TryAddRangeOrTransfer(list, false, false);
-			string letterText = this.GetLetterText(faction, list, randomInRange, site, site.parts.FirstOrDefault<SitePart>());
-			Find.LetterStack.ReceiveLetter(this.def.letterLabel, letterText, this.def.letterDef, site, faction, null);
-			return true;
-		}
+                return false;
+            }
+            var site = (Site)WorldObjectMaker.MakeWorldObject(WorldCoreDefOf.WorldOutpost);
+            site.Tile = tile;
+            site.AddPart(new SitePart(site, SiteCoreDefOf.OldOutpost, SiteCoreDefOf.OldOutpost.Worker.GenerateDefaultParams(StorytellerUtility.DefaultSiteThreatPointsNow(), tile, Find.FactionManager.RandomEnemyFaction(false, false, false, TechLevel.Industrial))));
+            site.SetFaction(Find.FactionManager.RandomEnemyFaction(false, false, false, TechLevel.Industrial));
+            site.GetComponent<TimeoutComp>().StartTimeout(TimeoutDaysRange.RandomInRange * 60000);
+            if (Find.WorldObjects != null) Find.WorldObjects.Add(site);
+            Find.LetterStack.ReceiveLetter("LetterLabelCrashedShip".Translate(), "LetterCrashedShip".Translate(), LetterDefOf.NeutralEvent, site, null);
 
-		protected virtual List<Thing> GenerateItems(Faction siteFaction, float siteThreatPoints)
-		{
-			ThingSetMakerParams thingSetMakerParams = default(ThingSetMakerParams);
-			return ThingSetMakerDefOf.Gen_OldOutpost.root.Generate(thingSetMakerParams);
-		}
-
-		public static Site CreateSite(int tile, SitePartDef sitePart, int days, Faction siteFaction)
-		{
-			Site site = SiteMaker.MakeSite(SiteCoreDefOf.OldOutpost, sitePart, tile, siteFaction, true, null);
-			site.sitePartsKnown = true;
-			site.GetComponent<TimeoutComp>().StartTimeout(days * 60000);
-			Find.WorldObjects.Add(site);
-			return site;
-		}
-
-		private string GetLetterText(Faction alliedFaction, List<Thing> items, int days, Site site, SitePart sitePart)
-		{
-			string result = GenText.CapitalizeFirst(GrammarResolverSimpleStringExtensions.Formatted(this.def.letterText, alliedFaction.leader.LabelShort, alliedFaction.def.leaderTitle, alliedFaction.Name, GenLabel.ThingsLabel(items, "  - "), days.ToString(), GenText.ToStringMoney(GenThing.GetMarketValue(items), null)));
-			return result;
-		}
-	}
+            return true;
+        }
+    }
 }
--- Source/PurpleIvyDLL/GenerationWorker/SiteCoreDefOf.cs
+++ Source/PurpleIvyDLL/GenerationWorker/SiteCoreDefOf.cs
@@ -3,13 +3,25 @@ using RimWorld;
 
 namespace GenerationWorker
 {
-	[DefOf]
-	public static class SiteCoreDefOf
-	{
-		static SiteCoreDefOf()
-		{
-			DefOfHelper.EnsureInitializedInCtor(typeof(SiteCoreDefOf));
-		}
+    [RimWorld.DefOf]
+    public static class SiteCoreDefOf
+    {
+        static SiteCoreDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(SiteCoreDefOf));
+        }
 
-	}
+        public static SitePartDef OldOutpost;
+    }
+    [RimWorld.DefOf]
+    public static class WorldCoreDefOf
+    {
+        static WorldCoreDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(WorldCoreDefOf));
+        }
+
+        public static WorldObjectDef WorldOutpost;
+    }
 }
+
