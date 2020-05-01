6c9bfdd1c906360bb1aaa3259392fe5ecd666e2a
Taranchuk <nfhfyxer@gmail.com>
2020-05-01 16:25:08 +0300

    New ship

--- Source/PurpleIvyDLL/Sites/GenStep_CrashedShip.cs
+++ Source/PurpleIvyDLL/Sites/GenStep_CrashedShip.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using RimWorld;
 using RimWorld.BaseGen;
 using Verse;
@@ -16,6 +18,11 @@ namespace PurpleIvy
         }
         public override void Generate(Map map, GenStepParams parms)
         {
+            var geoterms = map.listerThings.AllThings.Where(x => x.def == ThingDefOf.SteamGeyser).ToList();
+            for (int i = geoterms.Count - 1; i >= 0; i--)
+            {
+                geoterms[i].DeSpawn(DestroyMode.Vanish);
+            }
             int num = map.Size.x / 5;
             int num2 = 8 * map.Size.x / 5;
             int num3 = map.Size.z / 5;
@@ -26,18 +33,177 @@ namespace PurpleIvy
             CellFinder.TryFindRandomEdgeCellWith((IntVec3 v) =>
             GenGrid.Standable(v, map), map, 0f, out playerStartSpot);
             MapGenerator.PlayerStartSpot = playerStartSpot;
-            this.baseResolveParams = default(ResolveParams);
-            this.baseResolveParams.rect = this.adventureRegion;
+            this.rp = default(ResolveParams);
+            this.rp.rect = this.adventureRegion;
             BaseGen.globalSettings.map = map;
             BaseGen.globalSettings.minBuildings = 0;
             BaseGen.globalSettings.minBarracks = 0;
-            BaseGen.symbolStack.Push("crashedShip", this.baseResolveParams, null);
+
+            if (rp.wallStuff == null)
+            {
+                rp.wallStuff = ThingDefOf.Plasteel;
+            }
+            if (rp.floorDef == null)
+            {
+                rp.floorDef = TerrainDef.Named("SilverTile");
+            }
+
+            ResolveParams resolveParams = rp;
+            resolveParams.rect = new CellRect(rp.rect.minX, rp.rect.maxX / 2, 5, 13).ContractedBy(1);
+            resolveParams.thingRot = Rot4.North;
+            resolveParams.fillWithThingsPadding = new int?(resolveParams.fillWithThingsPadding ?? 1);
+            resolveParams.edgeThingAvoidOtherEdgeThings = new bool?(true);
+            BaseGen.symbolStack.Push("chargeBatteries", rp, null);
+            resolveParams.singleThingDef = ThingDefOf.Battery;
+            BaseGen.symbolStack.Push("fillWithThings", resolveParams, null);
+
+            ResolveParams batteryRoom = rp;
+            batteryRoom.rect = new CellRect(rp.rect.minX, rp.rect.maxX / 2, 5, 13);
+            BaseGen.symbolStack.Push("emptyRoom", batteryRoom, null);
+
+            // half way
+            ResolveParams halfway = rp;
+            halfway.rect = new CellRect(rp.rect.minX + 4, rp.rect.maxX / 2 + 4, 24, 5);
+            BaseGen.symbolStack.Push("emptyRoom", halfway, null);
+
+            //dining room
+            ResolveParams diningRoom = rp;
+            diningRoom.rect = new CellRect(rp.rect.minX + 4, rp.rect.maxX / 2, 8, 5);
+            BaseGen.symbolStack.Push("emptyRoom", diningRoom, null);
+
+            //generators
+            ResolveParams generators = rp;
+            generators.rect = new CellRect(rp.rect.minX + 4, rp.rect.maxX / 2 + 8, 8, 5);
+            BaseGen.symbolStack.Push("emptyRoom", generators, null);
+
+            // tech rooms
+            ResolveParams techRooms = rp;
+            techRooms.rect = new CellRect(rp.rect.minX + 10, rp.rect.maxX / 2, 18, 5);
+            BaseGen.symbolStack.Push("emptyRoom", techRooms, null);
+
+            // laboratory
+            ResolveParams laboratory = rp;
+            laboratory.rect = new CellRect(rp.rect.minX + 10, rp.rect.maxX / 2 + 8, 18, 5);
+            BaseGen.symbolStack.Push("emptyRoom", laboratory, null);
             BaseGen.Generate();
+            var doorsHalfway = new List<int>() { 4, 14, 25, 36, 46, 52 };
+            for (var i = 0; i < halfway.rect.EdgeCells.Count(); i++)
+            {
+                if (doorsHalfway.Contains(i))
+                {
+                    Log.Message(halfway.rect.EdgeCells.ToList()[i] + " position " + i);
+                    ThingDef stuff = ThingDefOf.Plasteel;
+                    Thing thing = ThingMaker.MakeThing(ThingDefOf.Door, stuff);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, halfway.rect.EdgeCells.ToList()[i], map);
+                }
+            }
+
+            for (var i = 0; i < diningRoom.rect.ToList().Count(); i++)
+            {
+                if (i == 25 || i == 26 || i == 29 || i == 30)
+                {
+                    ThingDef stuff = ThingDefOf.Steel;
+                    Thing thing = ThingMaker.MakeThing(ThingDefOf.DiningChair, stuff);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, diningRoom.rect.ToList()[i], map, Rot4.South);
+                }
+                else if (i == 10 || i == 9 || i == 14 || i == 13)
+                {
+                    ThingDef stuff = ThingDefOf.Steel;
+                    Thing thing = ThingMaker.MakeThing(ThingDefOf.DiningChair, stuff);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, diningRoom.rect.ToList()[i], map, Rot4.North);
+                }
+                else if (i == 18 ||i == 22)
+                {
+                    ThingDef stuff = ThingDefOf.Steel;
+                    Thing thing = ThingMaker.MakeThing(ThingDefOf.Table1x2c, stuff);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, diningRoom.rect.ToList()[i], map, Rot4.West);
+                }
+                Log.Message(diningRoom.rect.ToList()[i] + " diningRoom - position " + i);
+            }
+            var generatorsInd = new List<int>() { 17, 18, 19, 20, 21, 22 };
+
+            for (var i = 0; i < generators.rect.ToList().Count(); i++)
+            {
+                if (generatorsInd.Contains(i))
+                {
+                    Thing thing = ThingMaker.MakeThing(ThingDefOf.VanometricPowerCell, null);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, generators.rect.ToList()[i], map);
+                }
+            }
+
+            for (var i = 0; i < techRooms.rect.ToList().Count(); i++)
+            {
+                if (i == 22 || i == 46)
+                {
+                    ThingDef stuff = ThingDefOf.Steel;
+                    Thing thing = ThingMaker.MakeThing(ThingDefOf.DiningChair, stuff);
+                    thing.SetFaction(rp.faction, null);
+                    thing.Rotation = Rot4.North;
+                    GenSpawn.Spawn(thing, techRooms.rect.ToList()[i], map);
+                }
+                else if (i == 8)
+                {
+                    ThingDef stuff = ThingDefOf.Plasteel;
+                    Thing thing = ThingMaker.MakeThing(ThingDefOf.Door, stuff);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, techRooms.rect.ToList()[i], map);
+                }
+                else if (i == 64)
+                {
+                    ThingDef stuff = ThingDefOf.Steel;
+                    Thing thing = ThingMaker.MakeThing(ThingDef.Named("DrugLab"), stuff);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, techRooms.rect.ToList()[i], map);
+                }
+                else if (i == 40)
+                {
+                    ThingDef stuff = ThingDefOf.Steel;
+                    Thing thing = ThingMaker.MakeThing(ThingDef.Named("HiTechResearchBench"), stuff);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, techRooms.rect.ToList()[i], map);
+                }
+                else if (i == 48)
+                {
+                    Thing thing = ThingMaker.MakeThing(ThingDef.Named("MultiAnalyzer"), null);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, techRooms.rect.ToList()[i], map);
+                }
+                else if (i == 69)
+                {
+                    Thing thing = ThingMaker.MakeThing(ThingDef.Named("CommsConsole"), null);
+                    thing.SetFaction(rp.faction, null);
+                    GenSpawn.Spawn(thing, techRooms.rect.ToList()[i], map, Rot4.South);
+                }
+                Log.Message(techRooms.rect.ToList()[i] + " laboratory - position " + i);
+
+            }
+
+            for (var i = 0; i < laboratory.rect.ToList().Count(); i++)
+            {
+                if (i == 38 || i == 40 || i == 42)
+                {
+                    Thing thing = ThingMaker.MakeThing(PurpleIvyDefOf.PI_ContainmentBreach, null);
+                    thing.SetFaction(rp.faction, null);
+                    thing.Rotation = Rot4.North;
+                    GenSpawn.Spawn(thing, laboratory.rect.ToList()[i], map);
+                }
+                else if (i == 65 || i == 69 || i == 29 || i == 33)
+                {
+                    Thing thing = ThingMaker.MakeThing(ThingDef.Named("HydroponicsBasin"), null);
+                    thing.SetFaction(rp.faction, null);
+                    thing.Rotation = Rot4.West;
+                    GenSpawn.Spawn(thing, laboratory.rect.ToList()[i], map, Rot4.West);
+                }
+            }
         }
 
         protected CellRect adventureRegion;
 
-        protected ResolveParams baseResolveParams;
+        protected ResolveParams rp;
     }
 }
-
